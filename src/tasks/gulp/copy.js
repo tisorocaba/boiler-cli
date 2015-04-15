79f152f39e51c17798118d6abade9785225257ef
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2015-04-15 16:55:39 -0300

    correção na task copy.

--- src/tasks/gulp/copy.js
+++ src/tasks/gulp/copy.js
@@ -5,7 +5,7 @@ module.exports = function(applicationPath) {
 	var pkg = require(applicationPath('package.json'));
 
 	gulp.task('copy', function() {
-		gulp.src(['assets/**', '!assets/css/**']).pipe(gulp.dest('dist/' + pkg.version + '/assets'));
-		gulp.src(['config.json']).pipe(gulp.dest('dist/' + pkg.version));
+		gulp.src([applicationPath('assets/**'), applicationPath('!assets/css/**')]).pipe(gulp.dest(applicationPath('dist/' + pkg.version + '/assets')));
+		gulp.src([applicationPath('config.json')]).pipe(gulp.dest(applicationPath('dist/' + pkg.version)));
 	});
 }
