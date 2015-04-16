c447ac2e22e1824a07456615baeb22937691e34e
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2015-04-16 13:56:05 -0300

    verificação de segurança para impedir sobreescrita de arquivos na task init.

--- src/tasks/boiler/init.js
+++ src/tasks/boiler/init.js
@@ -1,31 +1,49 @@
 var shelljs = require('shelljs'),
-	colors = require('colors');
+	colors = require('colors'),
+	fs = require('fs');
 
 module.exports = function(applicationName) {
-	if(shelljs.which('git')) {
-		if(applicationName) {
-			shelljs.exec("git clone https://github.com/baltazzar/boiler " + applicationName, {silent: true});
-		} else {
-			shelljs.exec("git clone https://github.com/baltazzar/boiler .", {silent: true});
-		}
-	} else {
-		console.log('git not found! Using volo instead.'.cyan);
-		if(applicationName) {
-			shelljs.exec("volo create " + applicationName + " baltazzar/boiler");
-		} else {
-			shelljs.exec("volo install baltazzar/boiler . -f");
-		}
-	}
+
+	var folderIsEmpty = true;
 
 	if(applicationName) {
-		shelljs.cd(applicationName);
+		folderIsEmpty = fs.existsSync(applicationName) === false;
+	} else {
+		folderIsEmpty = fs.readdirSync('.').length === 0;
 	}
 
-	shelljs.rm('-rf', ['.gitignore', 'LICENSE', 'README.md', 'gulpfile.js', '.git']);
+	if(folderIsEmpty) {
+		if(shelljs.which('git')) {
+			if(applicationName) {
+				shelljs.exec("git clone https://github.com/baltazzar/boiler " + applicationName, {silent: true});
+			} else {
+				shelljs.exec("git clone https://github.com/baltazzar/boiler .", {silent: true});
+			}
+		} else {
+			console.log('git not found! Using volo instead.'.cyan);
+			if(applicationName) {
+				shelljs.exec("volo create " + applicationName + " baltazzar/boiler");
+			} else {
+				shelljs.exec("volo install baltazzar/boiler . -f");
+			}
+		}
 
-	if(applicationName) {
-		console.log('\napplication '.green + applicationName.cyan + ' created!'.green);
+		if(applicationName) {
+			shelljs.cd(applicationName);
+		}
+
+		shelljs.rm('-rf', ['.gitignore', 'LICENSE', 'README.md', '.git']);
+
+		if(applicationName) {
+			console.log('\napplication '.green + applicationName.cyan + ' created!'.green);
+		} else {
+			console.log('\napplication created!'.green);
+		}
 	} else {
-		console.log('\napplication created'.green);
+		if(applicationName) {
+			console.log('\nthe folder '.red + applicationName.cyan + ' is not empty!'.red);
+		} else {
+			console.log('\nthe current folder is not empty!'.red);
+		}
 	}
 }
