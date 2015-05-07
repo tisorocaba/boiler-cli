fa612f95a8cb926ca21720aad50501c1d7103a33
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2015-05-07 13:22:13 -0300

    correção ao copiar arquivos da pasta assets. Bump de versão.

--- src/tasks/gulp/copy.js
+++ src/tasks/gulp/copy.js
@@ -5,7 +5,7 @@ module.exports = function(applicationPath) {
 	var pkg = require(applicationPath('package.json'));
 
 	gulp.task('copy', function() {
-		gulp.src([applicationPath('assets/**'), applicationPath('!assets/css/**')]).pipe(gulp.dest(applicationPath('dist/' + pkg.version + '/assets')));
+		gulp.src([applicationPath('assets/**'), '!' + applicationPath('assets/css/**')]).pipe(gulp.dest(applicationPath('dist/' + pkg.version + '/assets')));
 		gulp.src([applicationPath('config.json')]).pipe(gulp.dest(applicationPath('dist/' + pkg.version)));
 	});
 }
