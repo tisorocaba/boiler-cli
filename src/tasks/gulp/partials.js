59bf4a0a85bb7a55385f5390c6268e198c0d0b6d
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2016-01-06 14:11:47 -0200

    bugfix in handlebars partials registration

--- src/tasks/gulp/partials.js
+++ src/tasks/gulp/partials.js
@@ -5,10 +5,12 @@ var gulp = require('gulp'),
 module.exports = function(applicationPath) {
 	gulp.task('partials', function() {
 		var partials = ["var Handlebars = require('handlebars/runtime')['default'];\n"];
+		var re = /application\/(.+)$/i;
 
 		glob.sync(applicationPath('application/templates/**/*.tpl')).forEach(function(file) {
 			if(file.indexOf('/_') !== -1) {
-				file = file.split('application/')[1];
+				file = re.exec(file)[1];
+
 				partials.push("Handlebars.registerPartial('" + file + "', require('" + file + "'));");
 			}
 		});
